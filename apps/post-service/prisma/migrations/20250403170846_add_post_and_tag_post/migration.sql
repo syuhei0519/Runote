e70b32392c66c41f5f0c5b1bd5a282e4deae65ac
s_fujioka <espoir519@yahoo.co.jp>
2025-04-04 02:18:49 +0900

    投稿にタグを追加

---
+++ apps/post-service/prisma/migrations/20250403170846_add_post_and_tag_post/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "PostTag" (
+    "postId" INTEGER NOT NULL,
+    "tagId" INTEGER NOT NULL,
+
+    CONSTRAINT "PostTag_pkey" PRIMARY KEY ("postId","tagId")
+);
+
+-- AddForeignKey
+ALTER TABLE "PostTag" ADD CONSTRAINT "PostTag_postId_fkey" FOREIGN KEY ("postId") REFERENCES "post"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
